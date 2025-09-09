4739ce7eb74974836f3337e6c8f00d236ac53770
paulo junior waquimaker <paulojrzti@gmail.com>
2025-09-09 20:17:57 -0300

    fix otimizando imagens

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"], // otimiza imagens
   },
+  reactStrictMode: true,
+  optimizeCss: true, // garante otimização do CSS
+  experimental: {
+    optimizeCss: true, // força uso do critters no App Router também
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "600", "700"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900",],
 });
 
 export const metadata: Metadata = {
