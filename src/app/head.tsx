488a5f376c697e4af270fa9ca7b659a4e5550801
paulo junior waquimaker <paulojrzti@gmail.com>
2025-09-09 22:19:17 -0300

    adiconando meta-tag-google

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,11 @@
-export default function Head() {
+import Head from "next/head";
+export default function Heade() {
   return (
-    <>
+    <Head>
       <meta
         name="google-site-verification"
         content="JDVMEekIXVLLcHVwgH5nj2vVb1qFOjA42Fx-EbuZukY"
       />
-    </>
+    </Head>
   );
 }
