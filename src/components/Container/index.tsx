3237ed1c5882e3aa24d28eac2423b211c1ae86a1
paulojrzti <paulojrzti@gmail.com>
2026-06-13 00:55:10 -0300

    fix: ajusta container principal para alinhar padding lateral do header com o resto do site

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,5 +2,5 @@ type ContainerProps = {
   children: React.ReactNode;
 };
 export function Container ({ children }: ContainerProps) {
-  return <div className="mx-auto max-w-5/6">{children}</div>;
+  return <div className="mx-auto w-[90%] max-w-[1100px] 2xl:max-w-[1300px]">{children}</div>;
 }
